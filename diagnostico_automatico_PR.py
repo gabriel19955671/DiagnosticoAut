bb88f9dac0a764e937502441ca8fe75cd2f25139
gabriel19955671 <gabriel.vilela@potencializeresultados.com>
2025-05-16 08:42:04 -0300

    Update diagnostico_automatico_PR.py

--- diagnostico_automatico_PR.py
+++ diagnostico_automatico_PR.py
@@ -118,4 +118,4 @@ if aba == "Cliente" and st.session_state.cliente_logado:
             insights.append("Desempenho da equipe pode estar comprometido.")
 
         diagnostico_texto = "
-".join(insights) if insights else "Nenhuma área crítica identificada. Excelente desempenho geral."  # corrigido".join(insights) if insights else "Nenhuma área crítica identificada. Excelente desempenho geral."
+".join(insights) if insights else "Nenhuma área crítica identificada. Excelente desempenho geral."".join(insights) if insights else "Nenhuma área crítica identificada. Excelente desempenho geral."
