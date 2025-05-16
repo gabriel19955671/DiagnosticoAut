04b52e4db08fd302455c138219419c3191aec909
gabriel19955671 <gabriel.vilela@potencializeresultados.com>
2025-05-16 10:34:28 -0300

    Update diagnostico_automatico_PR.py

--- diagnostico_automatico_PR.py
+++ diagnostico_automatico_PR.py
@@ -276,7 +276,7 @@ if aba == "Cliente":
                 "📝 Nome da sua empresa", value=user.iloc[0].get("Empresa", "Nome da Empresa")
             )
             nome = st.text_input("Nome do Responsável pelo preenchimento")
-            Telefone para contato = st.text_input("Telefone Whatsapp")
+            Telefone = st.text_input("Telefone Whatsapp")
             email = st.text_input("E-mail")
             financeiro = st.slider("Controle financeiro da empresa", 0, 10)
             processos = st.slider("Eficiência dos processos internos", 0, 10)
